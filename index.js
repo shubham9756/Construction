0f226f6cdf58f9a99c95cc0be7792e7d902bcd21
toshalauti2005 <autisunanda.1338@gmail.com>
2025-09-09 09:56:53 +0530

    oug
    gug

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var cookieParser = require("cookie-parser");
 var upload = require("express-fileupload");
 
 var adminroute = require("./router/admin");
-var site = require("./router/site");
-var godown = require("./router/godown");
 var userroute = require("./router/user");
 
 var app = express();
@@ -23,8 +21,6 @@ app.use(upload());
 
 app.use("/admin",adminroute);
 app.use("/",userroute);
-app.use("/site",site);
-app.use("/godown",godown);
 
 
 
