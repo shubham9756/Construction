090ed52fed2508e562f41c15c7dec55a7a3c5d75
shubham9756 <skarad819@gmail.com>
2025-09-12 19:48:09 +0530

    all

--- conn.js
+++ conn.js
@@ -1,11 +1,18 @@
 var mysql = require('mysql');
 var util = require('util');
 
+// var conn = mysql.createConnection({
+//   host: "b1fuygc9n4qhlfflu3qn-mysql.services.clever-cloud.com",
+//   user: "us2heyrjqh9uy5am",
+//   password: "7chkxc3xrwHnqAo1ISRH",
+//   database: "b1fuygc9n4qhlfflu3qn"
+// });
+
 var conn = mysql.createConnection({
-  host: "b1fuygc9n4qhlfflu3qn-mysql.services.clever-cloud.com",
-  user: "us2heyrjqh9uy5am",
-  password: "7chkxc3xrwHnqAo1ISRH",
-  database: "b1fuygc9n4qhlfflu3qn"
+  host: "localhost",  
+  user:'root',
+  password:'',
+  database:'construction'
 });
 
 var exe = util.promisify(conn.query).bind(conn);
--- router/admin.js
+++ router/admin.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", function (req, res) {
     res.render("admin/home.ejs");
 });
-
+// Site Management
 router.get("/addsite", function (req, res) {
     res.render("admin/addsite.ejs");
 });
@@ -19,63 +19,172 @@ router.post("/save_site", async function (req, res) {
 
         var filename1 = new Date().getTime() + req.files.site_3d_image.name;
         req.files.site_3d_image.mv("public/image/site_image/" + filename1);
-
-
-    }
-    var sql = `INSERT INTO site(site_name,site_location,site_manager_name,site_structure_image,site_3d_image,site_start_date,total_area_sqft,flats_per_floor,total_floors,total_flats,flat_area_sqft,site_completed_date,site_description)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)`;
-    var result = await exe(sql, [d.site_name, d.site_location, d.site_manager_name, filename, filename1, d.site_start_date, d.total_area_sqft, d.flats_per_floor, d.total_floors, d.total_flats, d.flat_area_sqft, d.site_completed_date, d.site_description]);
-
-
-    var sql1 = `INSERT INTO floor(site_id,flat_area_sqft)VALUES(?,?)`;
-    for (let i = 1; i <= d.total_floors; i++) {
-        var result1 = await exe(sql1, [result.insertId, d.flat_area_sqft]);
     }
 
-    res.redirect("/admin/addsite");
-
+    var sql = `INSERT INTO site(site_name,site_location,site_manager_name,site_structure_image,site_3d_image,site_start_date,site_description,site_map_link)VALUES(?,?,?,?,?,?,?,?)`;
+    var result = await exe(sql, [d.site_name, d.site_location, d.site_manager_name, filename, filename1, d.site_start_date, d.site_description, d.site_map_link]);
+    res.redirect("/addsite");
 });
-
-router.get("/pending_site", async function (req, res) {
-    var sql = `SELECT * FROM site WHERE status ='Pending'`;
+router.get("/site_list", async function (req, res) {
+    var sql = "SELECT * FROM site WHERE status ='Active'";
     var result = await exe(sql);
-    res.render("admin/pending_site.ejs", { data: result });
+    res.render("admin/site_list.ejs", { site: result });
 });
 
-router.get('/godwon_orders',async function (req,res){ 
-    res.render("admin/godwon_orders.ejs");
-    
+// Flat Management
+router.get('/add_new_selling_flat', async function (req, res) {
+    var sql = "SELECT * FROM site WHERE status='Active'";
+    var site = await exe(sql);
+    res.render('admin/add_flat.ejs', { site: site });
+});
+router.post('/save_flat', async function (req, res) {
+    var d = req.body;
+    var filename = "";
+    if (req.files) {
+        var filename = new Date().getTime() + req.files.flat_image.name;
+        req.files.flat_image.mv("public/image/flat/" + filename);
+    }
+    var sql = `INSERT INTO flats(flat_name,site_id,flat_image,carpet,buildup,description,type)VALUES(?,?,?,?,?,?,?)`;
+    var result = await exe(sql, [d.flat_name, d.site_name, filename, d.carpet, d.buldup, d.description, d.type]);
+    res.redirect('/add_new_selling_flat');
+});
+router.get('/new_selling_flat_list', async function (req, res) {
+    var sql = `SELECT * FROM flats INNER JOIN site ON flats.site_id = site.site_id WHERE flats.status='Available'`;
+    var flat = await exe(sql);
+    res.render('admin/flat_list.ejs', { flat });
 })
-router.post('/save_godown_order', async function (req, res) {
-        var d = req.body;
-        var sql = `INSERT INTO godown_orders 
-        (company_name, product_name, material_type, quantity, order_date, delivery_date, location, remarks, status) 
-        VALUES (?,?,?,?,?,?,?,?,?)`;
 
-        var result = await exe(sql, [
-            d.company_name,
-            d.product_name,
-            d.material_type,   
-            d.quantity,
-            d.order_date,
-            d.delivery_date,
-            d.location,
-            d.remarks,
-            "Pending"   // default status
-        ]);
-        res.redirect('/admin/godwon_orders');
-    
+
+// Rent Management
+router.get('/add_rent_flat', async function (req, res) {
+    var sql = `SELECT * FROM flats INNER JOIN site ON flats.site_id = site.site_id WHERE flats.type='Rent' AND flats.status='Available'`;
+    var flat = await exe(sql);
+    res.render('admin/rent_flat_list.ejs', { flat: flat });
+})
+
+
+// site flat view
+router.get('/view/:id', async function (req, res) {
+    var id = req.params.id;
+    var sql = "SELECT * FROM flats INNER JOIN site ON flats.site_id=site.site_id WHERE flats.flat_id = ?";
+    var employee = await exe("SELECT * FROM employees WHERE status='Active'");
+
+    var sql1 = "SELECT * FROM customers WHERE status='Active'";
+    var customer = await exe(sql1);
+    var result = await exe(sql, [id]);
+    res.render('admin/view_flat.ejs', { customer, result ,employee});
+});
+router.post('/flat-sold', async function (req, res) {
+    var d = req.body;
+
+    const sql = `INSERT INTO flat_sales ( customer_id,sale_date,invoice_no,deadline_date,carpet_sqft,buildup_sqft,sqfeet, rate,basic_amount,note,employee_signature,employee_id,customer_signature,customer_name,stamp_duty_percent,stamp_duty_amount,other_tax_persent,other_tax_amount,gst_percent,gst_amount,cgst_percent,cgst_amount,sgst_percent,sgst_amount,total_amount,discount_percent,discount_amount,grand_total) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+    var values = await exe(sql, [d.customer_id,d.date,d.invoice_no,d.deadline_date,d.carpet,d.buildup,d.sqfeet,d.rate,d.basic_amount,d.note,d.employee_signature,d.employee_id,d.customer_signature,d.customer_name,d.stamp_duty_percent,d.stamp_duty_amount,d.other_tax_percent,d.other_tax_amount,d.gst_percent,d.gst_amount,d.cgst_percent,d.cgst_amount,d.sgst_percent,d.sgst_amount,d.total_amount,d.discount_percent,d.discount_amount,d.grand_total ]);
+
+    var sql1 = "UPDATE flats SET status='Inavailable' WHERE flat_id=?";
+    res.redirect('/')
+
+
+
+})
+
+// sell management
+router.get('/add_selling_flat', async function (req, res) {
+    var sql = `SELECT * FROM flats INNER JOIN site ON flats.site_id = site.site_id WHERE flats.type='Sell'`;
+    var flat = await exe(sql);
+    res.render('admin/selling_flat_list.ejs', { flat: flat });
+})
+
+// customer management
+router.get("/add_customor", function (req, res) {
+    res.render("admin/add_customer.ejs");
+});
+router.post("/add_customor", async function (req, res) {
+    var d = req.body;
+    var sql = `INSERT INTO customers(full_name,email,mobile,password)VALUES(?,?,?,?)`;
+    var result = await exe(sql, [d.full_name, d.email, d.mobile, d.password]);
+    res.redirect("/add_customor");
+});
+router.get("/customor_list", async function (req, res) {
+    var sql = "SELECT * FROM customers WHERE status='Active'";
+    var result = await exe(sql);
+    console.log(result);
+    res.render("admin/customer_list.ejs", { customer: result });
 });
 
-router.get('/godwon_stock',async function (req,res){ 
-    var sql = `SELECT * FROM godown_orders`;
-    var result = await exe(sql);
-    res.render("admin/godwon_stock.ejs",{stock:result});   
-})  
 
-router.get('/godwon_order_stock',async function (req,res){
-    var sql = `SELECT * FROM godown_stock`;
-    res.render("admin/godwon_stock.ejs");   
-})  
+// material management
+router.get('/add_material', function (req, res) {
+    res.render('admin/add_material.ejs')
+})
+router.get('/order_material', async function (req, res) {
+    var sql = "SELECT * FROM materials";
+    var sql1 = "SELECT * FROM udm WHERE status='Active'";
+    var result1 = await exe(sql1);
+    var material = await exe(sql);
+    res.render('admin/material_order.ejs', { material, result1 });
+});
+router.post('/save_raw_material', function (req, res) {
+    var d = req.body
+    var sql = `INSERT INTO raw_material(company_name, raw_material_name, unit_weight, udm, purchase_amount, sale_address, gst_percentage)VALUES (?, ?, ?, ?, ?, ?, ?) `;
+    var result = exe(sql, [d.product_name, d.raw_material_name, d.unit_weight, d.udm, d.purchase_amount, d.sale_address, d.gst]);
+    res.redirect('/order_material');
+});
+router.get('/site_order_material', async function (req, res) {
+    var sql = "SELECT * FROM raw_material INNER JOIN udm ON raw_material.udm = udm.udm_id";
+    var material = await exe(sql);
+    console.log(material);
+    res.render('admin/material_stock.ejs', { material });
+})
+
+// udm management
+router.get('/unit', async function (req, res) {
+    var sql = "SELECT * FROM udm WHERE status='Active'";
+    var udm = await exe(sql);
+    res.render("admin/udm.ejs", { udm });
+})
+router.post('/save_udm', async function (req, res) {
+    var d = req.body;
+    var sql = `INSERT INTO udm(udm_name,udm_added_name)VALUES(?,?)`;
+    var result = await exe(sql, [d.udm_name, d.udm_added_name]);
+    res.redirect('/unit',);
+});
+router.get('/employee_list', function (req, res) {
+    res.render("admin/contractor_list.ejs");
+})
+
+// employee management
+router.get('/add_employee',async  function (req, res) {
+    var result = await exe("SELECT * FROM employee_types WHERE status='Active'");
+    res.render("admin/add_employee.ejs",{result});
+});
+router.post('/save_type', async function (req, res) {
+    var d = req.body;
+    var sql = `INSERT INTO employee_types(type_name,type_description)VALUES(?,?)`;
+    var result = await exe(sql, [d.type, d.details]);
+    res.redirect('/add_employee');
+});
+router.post('/save_employee', async function (req, res) {
+    var d = req.body;
+    var filename = "";
+    if (req.files) {
+        var filename = new Date().getTime() + req.files.employee_photo.name;
+        req.files.employee_photo.mv("public/image/employee/" + filename);
+    }
+    var sql = `INSERT INTO employees(employee_name,employee_email,employee_mobile,employee_address,employee_photo,employee_type_id,pan_number,aadhar_number,employee_password,employee_dob,employee_position,employee_in_time,employee_monthly_payment,employee_joining_date)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)`;
+    var result = await exe(sql, [d.employee_name, d.employee_email, d.employee_mobile, d.employee_address, filename, d.employee_type, d.pan_number, d.aadhar_number, d.employee_password, d.employee_dob, d.employee_position, d.employee_in_time, d.employee_monthly_payment, d.employee_joining_date]);
+    
+    res.redirect('/add_employee');
+});
+
+
+
+
+
+
+
+
+
+
 
 
 module.exports = router;
